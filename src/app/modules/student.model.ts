0225da0f805e65fa2fe4000765f714405dc2480e
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-11-22 23:53:06 +0600

    Create get Singlestudent route

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -12,42 +12,52 @@ import {
 const userNameSchema = new Schema<UserName>({
   firstName: {
     type: String,
+    required: true,
   },
   middleName: {
     type: String,
   },
   lastName: {
     type: String,
+    required: true,
   },
 });
 
 const gurdianSchema = new Schema<Guardian>({
   fatherName: {
     type: String,
+    required: true,
   },
   motherName: {
     type: String,
+    required: true,
   },
   motherOccupation: {
     type: String,
+    required: true,
   },
   motherContactNo: {
     type: String,
+    required: true,
   },
 });
 
 const localGuardianSchema = new Schema<LocalGuardian>({
   name: {
     type: String,
+    required: true,
   },
   occupation: {
     type: String,
+    required: true,
   },
   contactNo: {
     type: String,
+    required: true,
   },
   address: {
     type: String,
+    required: true,
   },
 });
 
@@ -61,19 +71,24 @@ const studentSchema = new Schema<Student>({
   },
   email: {
     type: String,
+    required: true,
   },
   contactNo: {
     type: String,
+    required: true,
   },
   emergencyContactNo: {
     type: String,
+    required: true,
   },
   bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
   presentAddress: {
     type: String,
+    required: true,
   },
   permanentAddress: {
     type: String,
+    required: true,
   },
   guardian: gurdianSchema,
 
--- src/app/modules/students/student.controller.ts
+++ src/app/modules/students/student.controller.ts
@@ -4,7 +4,7 @@ import { StudentServices } from './student.service';
 const createStudent = async (req: Request, res: Response) => {
   try {
     const { student: StudentData } = req.body;
-    console.log(StudentData);
+    // console.log(StudentData);
     const result = await StudentServices.createStudentIntoDB(StudentData);
 
     res.status(200).json({
@@ -16,6 +16,39 @@ const createStudent = async (req: Request, res: Response) => {
     console.log(error);
   }
 };
+// get controller
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Student are retrived successfully ',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// single id student findOne
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentsFromDB(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrived successfully ',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 export const StudentController = {
   createStudent,
+  getAllStudents,
+  getSingleStudent,
 };

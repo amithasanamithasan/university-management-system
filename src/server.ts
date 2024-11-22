ccde79f865e6f0355ef5baf8384e8bd4a73d136a
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-11-22 19:08:09 +0600

    create an interface Student typescript

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,11 @@ import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
 
-// getting-started.js
-// const mongo ose = require('mongoose');
-// npm i -D --exact prettier
-main().catch((err) => console.log(err));
-
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`Example is  listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);

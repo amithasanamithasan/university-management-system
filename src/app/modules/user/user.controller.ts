186adb76ad3eb86c86005a8e755d7cc3353fd9f8
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-12-14 13:48:52 +0600

    Avoid Repetition of Try-Catch , use catchAsync Higher order funtion

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,37 +1,26 @@
 import httpStatus from 'http-status-codes';
-import { NextFunction, Request, Response } from 'express';
 
 import { UserServices } from './user.service';
 import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student: StudentData } = req.body;
-    // console.log(StudentData);
-    const result = await UserServices.createStudentIntoDB(
-      password,
-      StudentData,
-    );
+const createStudent = catchAsync(async (req, res) => {
+  const { password, student: StudentData } = req.body;
+  // console.log(StudentData);
+  const result = await UserServices.createStudentIntoDB(password, StudentData);
 
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student is created successfully ',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is created succesfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Student is created successfully ',
+  //   data: result,
+  // });
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created succesfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,

d18dd5c407ad9d42ddee6ed591f514efdfd876f2
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-12-14 15:39:44 +0600

    Implement validateRequest Middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { UserControllers } from './user.controller';
+import validateRequest from '../../middleware/validateRequest';
+import { createStudentValidationSchema } from '../students/student.validation';
 
 const router = express.Router();
 
 // will call  controller funcation
 
-router.post('/create-student', UserControllers.createStudent);
+router.post(
+  '/create-student',
+  validateRequest(createStudentValidationSchema),
+  UserControllers.createStudent,
+);
 
 export const UserRoutes = router;

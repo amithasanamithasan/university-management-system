d1e5b08b1b9a22a13358e34b59880963ce927028
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-12-15 18:35:48 +0600

    Create Academic faculty  services created updateAcademicFacultyIntoDB

--- src/app/modules/academicFaculty/service.academicFaculty.ts
+++ src/app/modules/academicFaculty/service.academicFaculty.ts
@@ -13,8 +13,19 @@ const getSingleAcademicFacultyFromDB = async (id: string) => {
   const result = await AcademicFaculty.findById(id);
   return result;
 };
+
+const updateAcademicFacultyIntoDB = async (
+  id: string,
+  payload: Partial<TAcademicFaculty>,
+) => {
+  const result = await AcademicFaculty.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
 export const AcademicFacultyServices = {
   createAcademicFacultyIntoDB,
   getAllAcademicFacultiesFromDB,
   getSingleAcademicFacultyFromDB,
+  updateAcademicFacultyIntoDB,
 };

fee6f323cd5a0c88d5c41e05e20aee3ca22ddf1b
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-12-29 19:21:59 +0600

    Example is  listening on port 5000 test { userId: 'A-0001', role: 'admin', iat: 1735477554, exp: 1736341554 }

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -1,19 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import httpStatus from 'http-status-codes';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
+import config from '../config';
 
 const authmiddleware = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     // console.log({ token: req.headers.authorization });
     const token = req.headers.authorization;
     // console.log(token);
+
     // checking if the token is missing
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
     }
+
+    // checking if the given token is valid
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+    ) as JwtPayload;
+    // console.log(decoded);
+    req.user = decoded as JwtPayload;
+    const { role, userId, iat } = decoded;
     next();
   });
 };

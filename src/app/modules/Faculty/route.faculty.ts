28137444b2e106a2e6b24eb91ea1a29748efd8a1
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-02-08 13:36:08 +0600

    faculty route .ts faculty all access superadmin auth middelware

--- src/app/modules/Faculty/route.faculty.ts
+++ src/app/modules/Faculty/route.faculty.ts
@@ -2,24 +2,34 @@ import express from 'express';
 import { FacultyControllers } from './controller.faculty';
 import validateRequest from '../../middleware/validateRequest';
 import { updateFacultyValidationSchema } from './validation.faculty';
-import authmiddleware from '../../middleware/auth';
+
 import { USER_ROLE } from '../user/user.constant';
+import auth from '../../middleware/auth';
 
 const router = express.Router();
 
-router.get('/:id', FacultyControllers.getSingleFaculty);
+router.get(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
+  FacultyControllers.getSingleFaculty,
+);
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(updateFacultyValidationSchema),
   FacultyControllers.updateFaculty,
 );
 
-router.delete('/:id', FacultyControllers.deleteFaculty);
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  FacultyControllers.deleteFaculty,
+);
 
 router.get(
   '/',
-  authmiddleware(USER_ROLE.admin, USER_ROLE.faculty),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
   FacultyControllers.getAllFaculties,
 );
 
